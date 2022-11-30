990ce81915814c31c5f5f8279d86d4aea4be7e4f
Eloyse-Billy <eloysedantas22@gmail.com>
2022-11-30 17:14:17 -0300

    add: HomePage em MyApp

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:navy_flutter/pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: HomePage(),
+    );
   }
 }
