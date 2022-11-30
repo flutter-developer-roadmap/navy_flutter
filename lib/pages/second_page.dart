e7063781041bb442bf317203fc22c133c76098b1
Eloyse-Billy <eloysedantas22@gmail.com>
2022-11-30 16:55:55 -0300

    paginas

---
+++ lib/pages/second_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SecondPageState();
+  }
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
